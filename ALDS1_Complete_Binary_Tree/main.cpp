ccf137efe76683372672c1121dd8ae66ed51db45
kobutomo <n.tomoro13@gmail.com>
2021-01-09 23:41:10 +0900

    リファクタ

--- ALDS1_Complete_Binary_Tree/main.cpp
+++ ALDS1_Complete_Binary_Tree/main.cpp
@@ -1,15 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
-#define NIL 2000000001
 
-void print(int i, int H[]) {
+void print(int i, int max, int H[]) {
   printf("node %d: key = %d, ", i, H[i]);
-  if (H[i / 2] != NIL)
+  if (i / 2 >= 1)
     printf("parent key = %d, ", H[i / 2]);
-  if (H[i * 2] != NIL)
+  if (i * 2 <= max)
     printf("left key = %d, ", H[i * 2]);
-  if (H[i * 2 + 1] != NIL)
+  if (i * 2 + 1 <= max)
     printf("right key = %d, ", H[i * 2 + 1]);
   printf("\n");
 }
@@ -17,14 +16,11 @@ void print(int i, int H[]) {
 int main() {
   int n, k, H[501];
   cin >> n;
-  for (int & i : H) {
-    i = NIL;
-  }
   for (int i = 1; i < n + 1; ++i) {
     cin >> k;
     H[i] = k;
   }
   for (int i = 1; i < n + 1; ++i) {
-    print(i, H);
+    print(i, n, H);
   }
 }
