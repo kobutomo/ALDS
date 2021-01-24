d78deac62c0e017e2445b2f15489aa98871309fe
kobutomo <n.tomoro13@gmail.com>
2021-01-24 18:40:53 +0900

    Range

---
+++ ITP1_Range/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main() {
+  int a, b, c;
+  scanf("%d %d %d", &a, &b, &c);
+  if (b > a && c > b)
+    printf("Yes\n");
+  else
+    printf("No\n");
+}
