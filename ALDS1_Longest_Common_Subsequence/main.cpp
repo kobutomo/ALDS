d66497b2fe4689d079df99621e1cc1b7db5d78e4
kobutomo <n.tomoro13@gmail.com>
2021-01-26 02:54:28 +0900

    Longest Common Subsequence

---
+++ ALDS1_Longest_Common_Subsequence/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int LCS(string x, string y) {
+  int c[1001][1001];
+  int m = x.size(), n = y.size();
+  x = " " + x;
+  y = " " + y;
+  for (int j = 0; j < m; ++j) c[j][0] = 0;
+  for (int k = 0; k < n; ++k) c[k][0] = 0;
+  for (int i = 1; i <= m; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      if (x[i] == y[j]) {
+        c[i][j] = c[i - 1][j - 1] + 1;
+      } else {
+        c[i][j] = max(c[i - 1][j], c[i][j - 1]);
+      }
+    }
+  }
+  return c[m][n];
+}
+
+int main() {
+  int q;
+  string x, y;
+  scanf("%d", &q);
+  for (int i = 0; i < q; ++i) {
+    cin >> x >> y;
+    cout << LCS(x, y) << endl;
+  }
+}
