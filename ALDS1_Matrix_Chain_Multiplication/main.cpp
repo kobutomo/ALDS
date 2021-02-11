1aa982deb0cfcad16188f31739364c2fc46cc948
kobutomo <n.tomoro13@gmail.com>
2021-02-11 19:58:06 +0900

    連鎖行列積

---
+++ ALDS1_Matrix_Chain_Multiplication/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main() {
+  int n;
+  cin >> n;
+  int memo[n][n];
+  // 同じ行列同士の積はカウントしない。
+  for (int i = 0; i < n; ++i) memo[i][i] = 0;
+
+  vector<pair<int, int>> P(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> P.at(i).first >> P.at(i).second;
+  }
+  for (int r = 1; r < n; ++r) {
+    for (int i = 0; i < r; ++i) {
+      int l = r - i - 1;
+      memo[l][r] = 1 << 21;
+      for (int m = l; m < r; ++m) {
+        memo[l][r] = min(
+            memo[l][r],
+            memo[l][m]
+                + memo[m + 1][r]
+                + P.at(l).first * P.at(m).second * P.at(r).second
+        );
+      }
+    }
+  }
+  cout << memo[0][n - 1] << endl;
+}
