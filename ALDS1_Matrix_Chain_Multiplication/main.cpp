3c96aec92146e6f500975676b02d041a7a84f345
kobutomo <n.tomoro13@gmail.com>
2021-02-11 20:07:01 +0900

    lint ルール変えた

--- ALDS1_Matrix_Chain_Multiplication/main.cpp
+++ ALDS1_Matrix_Chain_Multiplication/main.cpp
@@ -11,21 +11,21 @@ int main() {
 
   vector<pair<int, int>> P(n);
   for (int i = 0; i < n; ++i) {
-    cin >> P.at(i).first >> P.at(i).second;
+	cin >> P.at(i).first >> P.at(i).second;
   }
   for (int r = 1; r < n; ++r) {
-    for (int i = 0; i < r; ++i) {
-      int l = r - i - 1;
-      memo[l][r] = 1 << 21;
-      for (int m = l; m < r; ++m) {
-        memo[l][r] = min(
-            memo[l][r],
-            memo[l][m]
-                + memo[m + 1][r]
-                + P.at(l).first * P.at(m).second * P.at(r).second
-        );
-      }
-    }
+	for (int i = 0; i < r; ++i) {
+	  int l = r - i - 1;
+	  memo[l][r] = 1 << 21;
+	  for (int m = l; m < r; ++m) {
+		memo[l][r] = min(
+			memo[l][r],
+			memo[l][m]
+				+ memo[m + 1][r]
+				+ P.at(l).first * P.at(m).second * P.at(r).second
+		);
+	  }
+	}
   }
   cout << memo[0][n - 1] << endl;
 }
