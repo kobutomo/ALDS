1ffc271706c107600b7577a32201f46fdfe35ae4
kobutomo <n.tomoro13@gmail.com>
2021-02-17 00:23:04 +0900

    連結成分分解

---
+++ ALDS1_Connected_Components/main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int n, m, s, t, q;
+int colors[100001];
+vector<vector<int>> G(100001);
+
+void dfs(int i, int color) {
+  colors[i] = color;
+  auto children = G.at(i);
+  for (int j = 0; j < children.size(); ++j) {
+    auto child = children.at(j);
+    if (colors[child] == 0) dfs(child, color);
+  }
+}
+
+int main() {
+  cin >> n >> m;
+  for (int i = 0; i < m; ++i) {
+    cin >> s >> t;
+    G.at(s).push_back(t);
+    G.at(t).push_back(s);
+  }
+  cin >> q;
+
+  for (int i = 0; i < n; ++i) {
+    if (colors[i] == 0)dfs(i, i + 1);
+  }
+
+  for (int i = 0; i < q; ++i) {
+    cin >> s >> t;
+    if (colors[s] == colors[t]) cout << "yes" << endl;
+    else cout << "no" << endl;
+  }
+  return 0;
+}
